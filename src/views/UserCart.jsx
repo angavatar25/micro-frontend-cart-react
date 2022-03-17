889816318ca6a4ad90bfa6bebb2646164bdbaf8c
Ananda Afryan Gemilang <anandalark007@gmail.com>
2022-03-17 10:58:52 +0700

    remove unused code

--- src/views/UserCart.jsx
+++ src/views/UserCart.jsx
@@ -3,37 +3,8 @@ import { useEffect, useState } from 'react'
 const rupiahChecker = (x) => {
     return 'Rp. ' + x.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")
 }
-const buttonQuantity = (quantity, flag) => {
-    if(flag === 'increment') {
-        quantity++
-    } else if (flag === 'decrement') {
-        quantity--
-    }
-}
-const CartComponent = ({product, quantity, increase, decrease, removeItem}) => product.map((index, i) => (
-    <div className="flex mb-6" key={i}>
-        <div className=" flex">
-            <div className=" w-16 h-16 bg-gray-500"></div>
-            <div className=" my-auto ml-4 flex justify-between">
-                <div>
-                    <p>{index.eventData.item}</p>
-                    <p>{rupiahChecker(index.eventData.price)}</p>
-                </div>
-                <div>
-                    <button onClick={decrease}>-</button>
-                    <p>{quantity}</p>
-                    <button onClick={increase}>+</button>
-                </div>
-                <div>
-                    <button onClick={removeItem}>Delete</button>
-                </div>
-            </div>
-        </div>
-    </div>
-))
 export default function UserCart() {
     const [cart, setcart] = useState([])
-    const [amount, setamount] = useState('')
     const [quantity, setquantity] = useState(0)
     const TestComponent = (product) => product.map((product, i) => (
         <div className="flex mb-6" key={i}>
@@ -62,7 +33,6 @@ export default function UserCart() {
             return
         }
         if(event.data.eventName === 'addToCart') {
-            //setitem(itemData.eventData)
             if(cart.some(e => e.eventData.item === itemData.eventData.item)) {
                 alert('Item already in cart')
             } else {
